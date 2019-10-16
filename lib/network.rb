73dc6c06d9dbfddd84497d8088d029d0ee50f629
msimon42 <msimon@uab.edu>
2019-10-16 11:01:32 -0600

    Add tests

--- lib/network.rb
+++ lib/network.rb
@@ -18,9 +18,16 @@ class Network
   end
 
   def highest_paid_actor
-    characters = @info[:shows].map {|show| show.characters}.flatten
-    max_character = characters.max_by {|character| character.salary}
-    max_character.actor 
+    @characters = @info[:shows].map {|show| show.characters}.flatten
+    max_character = @characters.max_by {|character| character.salary}
+    max_character.actor
   end
 
+  def payroll
+    payroll = Hash.new(0)
+    @characters.each do |character|
+      payroll[character.actor] = character[salary]
+    end
+    payroll
+  end
 end
--- test/network_test.rb
+++ test/network_test.rb
@@ -41,7 +41,7 @@ class NetworkTest < Minitest::Test
   def test_payroll
     @nbc.add_show(@knight_rider)
     @nbc.add_show(@parks_and_rec)
-
-    assert_equal {"David Hasselhoff" => 1600000, "William Daniels" => 1000000, "Amy Poehler" => 2000000, "Nick Offerman" => 1400000}, @nbc.payroll
-  end   
+    payroll = {"David Hasselhoff" => 1600000, "William Daniels" => 1000000, "Amy Poehler" => 2000000, "Nick Offerman" => 1400000}
+    assert_equal payroll , @nbc.payroll
+  end
 end
